3d1a8720f15027b82c7df91ac0225cb6125ad48e
Lewis Lemay <lewishlemay@gmail.com>
2025-10-26 16:06:10 -0600

    Add date idea list in array

---
+++ src/js/scripts.js
@@ -0,0 +1,39 @@
+//Declaring variables
+const dateIdeasList = [
+    //Paid ideas - index 0-10
+    "sushi battleship",
+    "movies & arcade",
+    "mini golf",
+    "visit the zoo",
+    "go to the mall",
+    "paint pottery",
+    "dollarama crafts",
+    "try something new",
+    "winners",
+    "Challenge 1: Buy things that are the other's favourite colour",
+    "Challenge 2: Choose the other's drink, snack, etc.",
+    //Free ideas - index 11-22
+    "listen to Taylor Swift records",
+    "minecraft",
+    "board games",
+    "wii games",
+    "work out",
+    "longboard",
+    "make bracelets",
+    "rust art class",
+    "watch Roey play video games",
+    "walk with Maggie",
+    "watch a show/youtube video",
+    "try something new"
+];
+
+
+
+
+//Function -- main
+async function main() {
+
+
+
+
+}
