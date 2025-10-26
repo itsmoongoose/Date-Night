475e468aa463f01f60976e30cf9354d2d6302ccb
Lewis Lemay <lewishlemay@gmail.com>
2025-10-26 17:01:02 -0600

    Add logic for generating random number for each button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,30 +1,30 @@
 //Declaring variables
 const dateIdeasList = [
     //Paid ideas - index 0-10
-    "sushi battleship",
-    "movies & arcade",
-    "mini golf",
-    "visit the zoo",
-    "go to the mall",
-    "paint pottery",
-    "dollarama crafts",
-    "try something new",
-    "winners",
+    "Sushi Battleship",
+    "Going to the Movies & Arcade",
+    "Mini Golf",
+    "Visiting the Zoo",
+    "Going to the Mall",
+    "Painting Pottery",
+    "Making Dollarama Crafts",
+    "Trying Something New",
+    "Going to Winners",
     "Challenge 1: Buy things that are the other's favourite colour",
     "Challenge 2: Choose the other's drink, snack, etc.",
     //Free ideas - index 11-22
-    "listen to Taylor Swift records",
-    "minecraft",
-    "board games",
-    "wii games",
-    "work out",
-    "longboard",
-    "make bracelets",
-    "rust art class",
-    "watch Roey play video games",
-    "walk with Maggie",
-    "watch a show/youtube video",
-    "try something new"
+    "Listening to Taylor Swift Records",
+    "Playing Minecraft",
+    "Playing Board Games",
+    "Playing Wii Games",
+    "Working Out",
+    "Longboarding",
+    "Making Bracelets",
+    "Doing a Rust Art Class",
+    "Watching Roey Play Video Games",
+    "Going on a Walk With Maggie",
+    "Watching a Show/YouTube Video",
+    "Trying Something New"
 ];
 const paidBtn = document.getElementById('paid-btn');
 const freeBtn = document.getElementById('free-btn');
@@ -35,17 +35,23 @@ async function main() {
 
     //Click -- paid button
     paidBtn.addEventListener('click', function() {
-
+        let randomNum = Math.floor(Math.random() * 11);
+        let dateIdea = dateIdeasList[randomNum];
+        alert("Winner! Our next date will be: " + dateIdea);
     });
 
     //Click -- free button
     freeBtn.addEventListener('click', function() {
-
+        let randomNum = Math.floor(Math.random() * 12) + 11;
+        let dateIdea = dateIdeasList[randomNum];
+        alert("Winner! Our next date will be: " + dateIdea);
     });
 
     //CLick -- random button
     randomBtn.addEventListener('click', function() {
-
+        let randomNum = Math.floor(Math.random() * 23);
+        let dateIdea = dateIdeasList[randomNum];
+        alert("Winner! Our next date will be: " + dateIdea);
     });
 
 
